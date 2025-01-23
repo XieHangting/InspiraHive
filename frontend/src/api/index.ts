73d8fcef1881ba77c96d0cc7b953cc218b58c368
XieHangting <39594504+XieHangting@users.noreply.github.com>
2025-01-23 12:19:43 +0800

    Update index.ts

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,19 +3,10 @@
 // API 更新时间：
 // API 唯一标识：
 import * as mainController from './mainController'
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default {
   mainController,
-=======
-=======
->>>>>>> 53246a2681b42fe1be0ba2bcde470c3e0d7905e6
 import * as userContoller from './userContoller'
 export default {
   mainController,
   userContoller,
-<<<<<<< HEAD
->>>>>>> 53246a2 (frontend user module)
-=======
->>>>>>> 53246a2681b42fe1be0ba2bcde470c3e0d7905e6
 }
