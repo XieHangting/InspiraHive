ed8991602a88ea562476d857ead4d27c8873680a
XieHangting <39594504+XieHangting@users.noreply.github.com>
2025-01-23 12:22:29 +0800

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HomePage from '@/pages/HomePage.vue'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import UserLoginPage from "@/pages/user/UserLoginPage.vue";
 import UserRegisterPage from "@/pages/user/UserRegisterPage.vue";
 import UserManagePage from "@/pages/admin/UserManagePage.vue";
->>>>>>> 53246a2 (frontend user module)
-=======
 import UserLoginPage from "@/pages/user/UserLoginPage.vue";
 import UserRegisterPage from "@/pages/user/UserRegisterPage.vue";
 import UserManagePage from "@/pages/admin/UserManagePage.vue";
->>>>>>> 53246a2681b42fe1be0ba2bcde470c3e0d7905e6
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,11 +17,6 @@ const router = createRouter({
       component: HomePage,
     },
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 53246a2681b42fe1be0ba2bcde470c3e0d7905e6
       path: '/user/login',
       name: '用户登录',
       component: UserLoginPage,
@@ -43,10 +32,6 @@ const router = createRouter({
       component: UserManagePage,
     },
     {
-<<<<<<< HEAD
->>>>>>> 53246a2 (frontend user module)
-=======
->>>>>>> 53246a2681b42fe1be0ba2bcde470c3e0d7905e6
       path: '/about',
       name: 'about',
       // route level code-splitting
